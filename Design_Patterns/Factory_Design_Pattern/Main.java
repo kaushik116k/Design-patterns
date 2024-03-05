40d3a791b1ad8e4ec3a28ecb6ad346e576ee2133
Kaushik Karamsetty <kaushik.karamsetty@BAN-38946L-B2.local>
2024-03-06 00:42:32 +0530

    factory design pattern

--- Design_Patterns/Factory_Design_Pattern/Main.java
+++ Design_Patterns/Factory_Design_Pattern/Main.java
@@ -2,14 +2,15 @@ package Design_Patterns.Factory_Design_Pattern;
 
 import java.util.Scanner;
 
+/// factory design pattern
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("please enter the vehicle type \'car\' or \'bike\'");
         String vehicleType = scanner.next();
-        GetVehicleType vehicleObject = new GetVehicleType();
-        vehicle vehicle = vehicleObject.getVehicle(vehicleType);
+        VehicleFactoryType vehicleFactoryType = new VehicleFactoryType();
+        vehicle vehicle = vehicleFactoryType.getVehicle(vehicleType);
         vehicle.createVehicle();
     }
 }
