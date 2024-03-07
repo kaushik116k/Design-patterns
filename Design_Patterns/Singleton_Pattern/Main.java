3de9a07b209695998859d2022012985ba15c494b
Kaushik Karamsetty <kaushik.karamsetty@BAN-38946L-B2.local>
2024-03-07 15:41:15 +0530

    singleton design pattern

---
+++ Design_Patterns/Singleton_Pattern/Main.java
@@ -0,0 +1,31 @@
+package Design_Patterns.Singleton_Pattern;
+
+class Logger {
+    private Logger() {
+        System.out.println("Logger instance is created");
+    };
+
+    static Logger LoggerInstance;
+
+    public static Logger createInstance() {
+        if(LoggerInstance == null) {
+            LoggerInstance = new Logger();
+        }
+
+        return LoggerInstance;
+    }
+
+    public void LogMessage(String msg) {
+        System.out.println(msg);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.createInstance();
+        logger1.LogMessage("This msg is from user 1");
+
+        Logger logger2 = Logger.createInstance();
+        logger2.LogMessage("This msg is from user 2");
+    }    
+}
